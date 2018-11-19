2302ab834d77e800768aa06e6717883535ff7721
Black Jack <hibbarcd@gmail.com>
2018-11-18 22:16:55 -0600

    mobile friendly fixes added

--- src/components/CharacterCard/CharacterCard.js
+++ src/components/CharacterCard/CharacterCard.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./CharacterCard.css";
 
 const CharacterCard = props => (
-	<div onClick={() => props.clickCard(props)} className="card col-sm-3 p-3 m-4  animated bounceIn hoverable">
+	<div onClick={() => props.clickCard(props)} className="card col-sm-3 m-4 animated bounceIn hoverable">
 		<img className="card-img-top" src={props.image} alt={props.name} />
 	</div>
 );
